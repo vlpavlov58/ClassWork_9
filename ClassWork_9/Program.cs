b06f6b6610c9fd81b6a8140a9af54f3ecd257168
Vladislav <36709482+vlpavlov58@users.noreply.github.com>
2018-03-12 22:39:39 +0200

    Add files via upload

---
+++ ClassWork_9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_9
+{
+    class Program
+    {
+        //факториал методом
+        static int GetFactorial(int x)  
+        {
+            int result = 1;
+            for (int i = 2; i <= x; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFactorial(10));
+           
+            
+        }
+    }
+}
